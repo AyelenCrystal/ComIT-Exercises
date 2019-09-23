225c7d353b3c1972df58bd5e3fa7a2fac9799c9a
ayelencrystal <ayelen.crystal@gmail.com>
2019-09-23 18:39:55 -0300

    Agregar funciones de resta, multiplicacion y division

--- api-calculos/server.js
+++ api-calculos/server.js
@@ -9,30 +9,37 @@ app.use(express.static(path.join(__dirname,`/index`)));
 
 app.get("/",(require, response)=>{
     response.sendFile(path.join(__dirname, `/index.html`));
-
 });
 
 app.get("/adding", (require, response)=>{
-    // response.sendFile(path.join(__dirname,"/adding.html"));
     var req = require.query;
+    //Parsea un argumento y devuelve un numero. Repetir en todas la ecuaciones
     response.send({
         result: (parseFloat(req.num1) + parseFloat(req.num2))
     });
 });
 
 app.get("/subtract",(require, response)=>{
-    response.sendFile(path.join(__dirname,"/subtract.html"));
-    response.send("num1" - "num2");
+    var req = require.query;
+    response.send({
+        result: (parseFloat(req.num1) - parseFloat(req.num2))
+    });
 });
 
 app.get("/multiply",(require, response)=>{
-    response.sendFile(path.join(__dirname,"/multiply.html"));
-    response.send("num1" * "num2");
+    var req = require.query;
+    response.send({
+        result: (parseFloat(req.num1) * parseFloat(req.num2))
+    });
+    
 });
 
 app.get("/divide",(require, response)=>{
-    response.sendFile(path.join(__dirname,"/divide.html"))
-    response.send("num1" / "num2");
+    var req = require.query;
+    response.send({
+        result: (parseFloat(req.num1) / parseFloat(req.num2))
+    });
+    
 });
 
 app.listen(5000,() => {
